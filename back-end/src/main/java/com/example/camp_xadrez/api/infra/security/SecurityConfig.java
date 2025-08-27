ea8bb9fcf877bcdaa076fc0d1cfc9465d880bdeb
caiovazeredo <caio.viana.39@gmail.com>
2025-08-27 12:51:50 -0300

    adição dos arquivos de autenticacao

--- back-end/src/main/java/com/example/camp_xadrez/api/infra/security/SecurityConfig.java
+++ back-end/src/main/java/com/example/camp_xadrez/api/infra/security/SecurityConfig.java
@@ -1,20 +1,30 @@
 package com.example.camp_xadrez.api.infra.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // desativa CSRF (opcional p/ APIs REST)
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         // libera swagger
                         .requestMatchers(
@@ -28,9 +38,19 @@ public class SecurityConfig {
                         // qualquer outra requisição exige login
                         .anyRequest().authenticated()
                 )
-                // habilita Basic Auth
-                .httpBasic(basic -> {});
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .builder();
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- back-end/src/test/java/com/example/camp_xadrez/api/controller/AlunoControllerTest.java
+++ back-end/src/test/java/com/example/camp_xadrez/api/controller/AlunoControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
