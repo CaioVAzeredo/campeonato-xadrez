9b72481783120540d7bf1e28a41697a2458795f9
caiovazeredo <caio.viana.39@gmail.com>
2025-08-29 22:33:46 -0300

    adição do endpoint de autenticacao

--- back-end/src/main/java/com/example/camp_xadrez/api/controller/AuthController.java
+++ back-end/src/main/java/com/example/camp_xadrez/api/controller/AuthController.java
@@ -1,8 +1,35 @@
 package com.example.camp_xadrez.api.controller;
 
+import com.example.camp_xadrez.api.domain.professor.DadosAutenticacao;
+import com.example.camp_xadrez.api.domain.professor.Professor;
+import com.example.camp_xadrez.api.infra.security.DadosTokenJWT;
+import com.example.camp_xadrez.api.infra.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<DadosTokenJWT> login(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Professor) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
 }
---
+++ back-end/src/main/java/com/example/camp_xadrez/api/domain/professor/AutenticacaoService.java
@@ -0,0 +1,15 @@
+package com.example.camp_xadrez.api.domain.professor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticacaoService implements UserDetailsService {
+    @Autowired
+    private ProfessorRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+}
